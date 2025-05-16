cdd59d781ebb621a8b302fd65d42e083b7ad8698
storm8787 <baechu1310@gmail.com>
2025-05-16 17:50:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,13 +19,16 @@ def main():
     st.sidebar.title("🧰 기능 선택")
     selected_app = st.sidebar.radio("아래 기능 중 선택하세요", [
         "(생성형AI) 보도자료 생성기",
+        "(업무자동화) 일정등록",
         "(업무자동화) 엑셀 취합기"
     ])
 
     if selected_app == "(생성형AI) 보도자료 생성기":
         press_release_app()
+    elif selected_app == "(업무자동화) 일정등록":
+        calendar_app()
     elif selected_app == "(업무자동화) 엑셀 취합기":
-        excel_merger()
+        excel_merger()        
 
 if __name__ == "__main__":
     main()

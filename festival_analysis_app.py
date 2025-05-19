2817615ec965b47f97edf300436c0e06e41de41e
storm8787 <baechu1310@gmail.com>
2025-05-19 14:44:52 +0900

    Update festival_analysis_app.py

--- festival_analysis_app.py
+++ festival_analysis_app.py
@@ -55,22 +55,22 @@ def festival_analysis_app():
 
     # ✅ 항목별 실행
     if selected_analysis == "1. 축제 방문객 현황(총괄)":
-        from festival.analyze_summary import analyze_summary
+        from press_release_app.festival.analyze_summary import analyze_summary
         analyze_summary()
     elif selected_analysis == "2. 축제 일자별 방문객 수":
-        from festival.analyze_daily_visitor import analyze_daily_visitor
+        from press_release_app.festival.analyze_daily_visitor import analyze_daily_visitor
         analyze_daily_visitor()
     elif selected_analysis == "3. 시간대별 관광객 존재 현황":
-        from festival.analyze_time_distribution import analyze_time_distribution
+        from press_release_app.festival.analyze_time_distribution import analyze_time_distribution
         analyze_time_distribution()
     elif selected_analysis == "4. 축제 전·중·후 방문객 분석":
-        from festival.analyze_before_after import analyze_before_after
+        from press_release_app.festival.analyze_before_after import analyze_before_after
         analyze_before_after()
     elif selected_analysis == "5. 연령별 방문객 분석":
-        from festival.analyze_age_group import analyze_age_group
+        from press_release_app.festival.analyze_age_group import analyze_age_group
         analyze_age_group()
     elif selected_analysis == "6. 외지인 이동지역 분석":
-        from festival.analyze_mobility import analyze_mobility
+        from press_release_app.festival.analyze_mobility import analyze_mobility
         analyze_mobility()
 
 

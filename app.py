3b007f852141be7ee06e9c3d700e2ccc6dcc9999
storm8787 <baechu1310@gmail.com>
2025-05-28 15:32:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -85,9 +85,8 @@ def main():
         data_validator_app()    
     elif st.session_state.selected_app == "(생성형AI) 공적조서 생성기":        
         create_official_merit_report()
-    elif st.session_state.selected_app == "(생성형AI) 업무보고 생성기":
-        report_writer_app()
-    elif st.session_state.selecte
+    elif st.session_state.selected_app == "(생성형AI) 업무보고 생성기(개발중)":
+        report_writer_app()        
 
 if __name__ == "__main__":
     main()
